5490dba3d92bad1fabd02d3852c11190dd14dc03
JaquelineTorres <jaqueline.torres.perez.unam@gmail.com>
2024-01-05 15:58:09 -0600

    Added comments for documentation

--- HolaMundoJava/src/WhileLoop.java
+++ HolaMundoJava/src/WhileLoop.java
@@ -1,4 +1,7 @@
 public class WhileLoop {
+    /**
+     * Description: variable global que indica el estado de la lámpara del celular
+     */
     static boolean isTurnOnLight = false;
 
     public static void main(String[] args) {
@@ -11,10 +14,19 @@ public class WhileLoop {
         }
     }
 
+    /**
+     * Description: función que imprime el mensaje SOS en clave morse
+     */
     public static void printSOS() {
         System.out.println("··· — — — ···");
     }
 
+    /**
+     * Description: indica si la lámpara del celular se encuentra encendida o no
+     *
+     * @return isTurnOnLight
+     * @author Jaqueline
+     */
     public static boolean turnOnLight() {
         isTurnOnLight = (isTurnOnLight) ? false : true;
         return isTurnOnLight;
